604ef9486955fb9b26c06c5d971858af0c4c1d8d
Joshua Corney <joshua.corney@hotmail.com>
2015-02-09 17:52:19 -0800

    Master

---
+++ boomgaarden_corney.android.light/gen/com/boomgaarden_corney/android/light/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.boomgaarden_corney.android.light;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
